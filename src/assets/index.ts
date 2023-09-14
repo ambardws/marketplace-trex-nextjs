e44a74359c3ecb93fffd7b5e238e1660f9993794
aqigif <aqilgifari0@gmail.com>
2023-09-14 11:40:11 +0700

    feat: assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,15 @@
-export {
-    
-}
+import Logo from "./images/logo.png";
+
+import Cart from "./icons/cart.svg";
+import MenuHamburger from "/icons/menu-hamburger.svg";
+
+const assets = {
+  TrexImages: {
+    Logo,
+  },
+  TrexIcons: {
+    Cart,
+    MenuHamburger,
+  },
+};
+export default assets;

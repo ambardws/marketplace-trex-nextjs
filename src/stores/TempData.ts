6620bf460bda54a080fa09cadac9036979b49fe5
Ambar Dwi Saputra <ambardwisaputra@gmail.com>
2023-10-02 11:44:35 +0700

    add page order lists

--- src/stores/TempData.ts
+++ src/stores/TempData.ts
@@ -157,3 +157,162 @@ export const datasCheckout = {
     },
   ],
 };
+
+export const ListOrders = [
+  {
+    kodeTransaksi: "O - 1234 - 1234",
+    date: "20 Jan 2022 10:44",
+    orders: [
+      {
+        items: [
+          {
+            id: 1,
+            title: "Samsung Galaxy S22 Ultra RAM 8gb Internal 250gb",
+            image: assets.TrexImages.Samsung,
+            price: 2500000,
+            margin: 20000,
+            available: 85,
+            variaty: 8 / 128,
+            merk: "Samsung",
+            category: "Handphone",
+            kode: 8771231,
+            dimension: "57 x 49 x 10",
+            weight: "0. 75kg",
+            fromCity: "Tangerang",
+            count: 1,
+            description: `Ukuran layar : 6.6 inches, 104.9 cm2 (~82.5% screen-to-body ratio) 1080 x 2408 pixels, 20:9 ratio (~400 ppi density)
+                          Chipset Qualcomm SM6225 Snapdragon 680 4G (6 nm) dengan CPU Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)`,
+          },
+          {
+            id: 1,
+            title: "Samsung Galaxy S22 Ultra RAM 8gb Internal 250gb",
+            image: assets.TrexImages.Samsung,
+            price: 2500000,
+            margin: 20000,
+            available: 85,
+            variaty: 8 / 128,
+            merk: "Samsung",
+            category: "Handphone",
+            kode: 8771231,
+            dimension: "57 x 49 x 10",
+            weight: "0. 75kg",
+            fromCity: "Tangerang",
+            count: 1,
+            description: `Ukuran layar : 6.6 inches, 104.9 cm2 (~82.5% screen-to-body ratio) 1080 x 2408 pixels, 20:9 ratio (~400 ppi density)
+                          Chipset Qualcomm SM6225 Snapdragon 680 4G (6 nm) dengan CPU Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)`,
+          },
+        ],
+        method: "Diantar Kerumah",
+        status: "Dikirim",
+        totalPrice: 2999000,
+      },
+      {
+        items: [
+          {
+            id: 2,
+            title: "Samsung Galaxy S20 Ultra RAM 8gb Internal 250gb",
+            image: assets.TrexImages.Samsung,
+            price: 2500000,
+            margin: 20000,
+            available: 85,
+            variaty: 6 / 12,
+            merk: "Samsung",
+            category: "Handphone",
+            kode: 8771231,
+            dimension: "57 x 49 x 10",
+            weight: "0. 75kg",
+            fromCity: "Tangerang",
+            count: 1,
+            description: `Ukuran layar : 6.6 inches, 104.9 cm2 (~82.5% screen-to-body ratio) 1080 x 2408 pixels, 20:9 ratio (~400 ppi density)
+                      Chipset Qualcomm SM6225 Snapdragon 680 4G (6 nm) dengan CPU Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)`,
+          },
+        ],
+        method: "Ambil Ditoko",
+        status: "Menunggu Pembayaran",
+        totalPrice: 1999000,
+      },
+    ],
+  },
+  {
+    kodeTransaksi: "O - 1234 - 1234",
+    date: "20 Jan 2022 10:44",
+    orders: [
+      {
+        items: [
+          {
+            id: 1,
+            title: "Samsung Galaxy S22 Ultra RAM 8gb Internal 250gb",
+            image: assets.TrexImages.Samsung,
+            price: 2500000,
+            margin: 20000,
+            available: 85,
+            variaty: 8 / 128,
+            merk: "Samsung",
+            category: "Handphone",
+            kode: 8771231,
+            dimension: "57 x 49 x 10",
+            weight: "0. 75kg",
+            fromCity: "Tangerang",
+            count: 1,
+            description: `Ukuran layar : 6.6 inches, 104.9 cm2 (~82.5% screen-to-body ratio) 1080 x 2408 pixels, 20:9 ratio (~400 ppi density)
+                          Chipset Qualcomm SM6225 Snapdragon 680 4G (6 nm) dengan CPU Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)`,
+          },
+          {
+            id: 2,
+            title: "Samsung Galaxy S22 Ultra RAM 8gb Internal 250gb",
+            image: assets.TrexImages.Samsung,
+            price: 2500000,
+            margin: 20000,
+            available: 85,
+            variaty: 8 / 128,
+            merk: "Samsung",
+            category: "Handphone",
+            kode: 8771231,
+            dimension: "57 x 49 x 10",
+            weight: "0. 75kg",
+            fromCity: "Tangerang",
+            count: 1,
+            description: `Ukuran layar : 6.6 inches, 104.9 cm2 (~82.5% screen-to-body ratio) 1080 x 2408 pixels, 20:9 ratio (~400 ppi density)
+                          Chipset Qualcomm SM6225 Snapdragon 680 4G (6 nm) dengan CPU Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)`,
+          },
+        ],
+        method: "Delivery",
+        status: "Di Proses",
+        totalPrice: 2999000,
+      },
+      {
+        items: [
+          {
+            id: 2,
+            title: "Samsung Galaxy S20 Ultra RAM 8gb Internal 250gb",
+            image: assets.TrexImages.Samsung,
+            price: 2500000,
+            margin: 20000,
+            available: 85,
+            variaty: 6 / 12,
+            merk: "Samsung",
+            category: "Handphone",
+            kode: 8771231,
+            dimension: "57 x 49 x 10",
+            weight: "0. 75kg",
+            fromCity: "Tangerang",
+            count: 1,
+            description: `Ukuran layar : 6.6 inches, 104.9 cm2 (~82.5% screen-to-body ratio) 1080 x 2408 pixels, 20:9 ratio (~400 ppi density)
+                      Chipset Qualcomm SM6225 Snapdragon 680 4G (6 nm) dengan CPU Octa-core (4x2.4 GHz Kryo 265 Gold & 4x1.9 GHz Kryo 265 Silver)`,
+          },
+        ],
+        method: "COD",
+        status: "Selesai",
+        totalPrice: 1999000,
+      },
+    ],
+  },
+];
+
+export const classStatus = {
+  deliver: `bg-info text-secondary`,
+  waitingPayment: `bg-base-200 text-slate-400`,
+  process: `bg-orange-100 text-orange-500`,
+  finished: `bg-green-100 text-success`,
+};
+
